40ed663e964685fe60604a28e2e8a8a590ac0125
Eddi1995 <edwinanthony513@gmail.com>
2023-05-24 15:05:28 +0530

    some changes

--- zealhubit-app/src/Components/Home/Home.js
+++ zealhubit-app/src/Components/Home/Home.js
@@ -1,10 +1,20 @@
-
+import mainstyle from "./Home.module.css"
 
 
 function Home(){
     return(
         <>
-        <h1>Home</h1>
+       <main class={mainstyle.mainParent}>   {/* parent */}
+            <div>      {/* child1 */}
+            <h2>Development that easy!</h2>
+            <h5>Apply disruptive technologies to transform digital business growth with zealhub IT.</h5>
+
+            </div>
+            <div>      {/* child2 */}
+            hellow2
+
+            </div>
+        </main>
         </>
     )
 }
