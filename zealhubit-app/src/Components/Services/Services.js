d17ab58fee8833ff1fc17f8a5185fcff207437f9
Eddi1995 <edwinanthony513@gmail.com>
2023-05-28 03:18:19 +0530

    update7

--- zealhubit-app/src/Components/Services/Services.js
+++ zealhubit-app/src/Components/Services/Services.js
@@ -1,68 +1,70 @@
 import servicestyle from "./Services.module.css"
+import Tilt from "react-parallax-tilt"   //By using this import only tilt can be done refer 
 
 
 function Services(){
     return(
         <>
-        <h2>Services</h2>
+        <h2 class={servicestyle.serviceMainHeading} ><b>Services</b></h2>
 
-      <section className={servicestyle.serviceParent}>    {/* parent */}
-            <div>   {/*child1 */}
-            <div class="card" >
-           <img src="./Assetz/frontend5.jpg" class="card-img-top" alt="frontend"/>
-          <div class="card-body">
-           <h5 class="card-title">Web Development</h5>
-         <p class="card-text">Offering profound innovation information and wide broadness of experience over different enterprises to assist customers with their custom web application development and support activities.</p>
-         <a href="#" class="btn btn-warning">Touch me for more info</a>
+      <section class={servicestyle.serviceParent}>    {/* parent */}
+            <Tilt >   {/*child1 */}
+            <div className="card border border-warning rounded" >
+           <img src="./Assetz/frontend5.jpg"  className="card-img-top" class={servicestyle.cardImageOne} />
+          <div className="card-body ">
+           <h5 className="card-title text-warning">Web Development</h5>
+         <p className="card-text">Innovation information and wide broadness of experience over different enterprises to assist customers with their custom web application development and support.</p>
+         <a href="#" className="btn btn-warning">Click here for more info</a>
           </div>
          </div>
   
-            </div>
-            <div> {/*child2 */}
-            <div class="card" >
-           <img src="./Assetz/mobdeveloopment.png" class="card-img-top" alt="mobdevelop"/>
-          <div class="card-body">
-           <h5 class="card-title">Mobile Development</h5>
-         <p class="card-text">We provide top notch mobile app development solutions. With capabilities in Hybrid and Native mobile apps we help our clients.</p>
-         <a href="#" class="btn btn-warning">Touch me for more info</a>
+            </Tilt>
+            <Tilt> {/*child2 */}
+            <div className="card" >
+           <img src="./Assetz/mobdeveloopment.png" className="card-img-top" alt="mobdevelop"/>
+           
+          <div className="card-body">
+           <h5 className="card-title text-warning">Mobile Development</h5>
+         <p className="card-text">We provide top notch mobile app development in a variety of programming languages solutions. With capabilities in Hybrid and Native mobile apps we help our clients.</p>
+         <a href="#" className="btn btn-warning">Click here for more info</a>
           </div>
          </div>
             
-            </div>
-            <div>   {/*child3 */}
-            <div class="card" >
-           <img src="./Assetz/frontend1.jpg" class="card-img-top" alt="pythonImage"/>
-          <div class="card-body">
-           <h5 class="card-title">Python</h5>
-         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-         <a href="#" class="btn btn-warning">Touch me for more info</a>
+            </Tilt>
+            <Tilt>   {/*child3 */}
+            <div className="card border border-warning" >
+           <img src="./Assetz/frontend1.jpg" className="card-img-top" alt="pythonImage"/>
+          <div className="card-body">
+           <h5 className="card-title text-warning">Python</h5>
+         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+         <a href="#" className="btn btn-warning">Click here for more info</a>
           </div>
          </div>
-            </div>
+            </Tilt>
 
-            <div>    {/*child4 */}
+            <Tilt>    {/*child4 */}
                 
-            <div class="card" >
-           <img src="./Assetz/devops.jpg" class="card-img-top" alt="devopsImage"/>
-          <div class="card-body">
-           <h5 class="card-title">DevOps Services</h5>
-         <p class="card-text">Our devops services aim at providing an automated practices which helps bridge the gap between development and operations.</p>
-          <a href="#" class="btn btn-warning">Touch me for more info</a>
+            <div className="card" >
+           <img src="./Assetz/devops.jpg" className="card-img-top" alt="devopsImage"/>
+          <div className="card-body">
+           <h5 className="card-title text-warning">DevOps Services</h5>
+         <p className="card-text">Our devops services aim at providing an automated using stackable virtual development practices which helps bridge the gap between development and operation.</p>
+          <a href="#" className="btn btn-warning">Click here for more info</a>
           </div>
          </div>
-            </div>
+            </Tilt>
 
-            <div>    {/*child5 */}
+            <Tilt>    {/*child5 */}
                 
-                <div class="card" >
-               <img src="./Assetz/bulkmail.jpg" class="card-img-top" alt="mailphot"/>
-              <div class="card-body">
-               <h5 class="card-title">Bulk Mailing</h5>
-             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-             <a href="#" class="btn btn-warning">Touch me for more info</a>
+                <div className="card border border-warning" >
+               <img src="./Assetz/bulkmailone.jpg" className="card-img-top" alt="mailphot"/>
+              <div className="card-body">
+               <h5 className="card-title text-warning">Bulk Mailing</h5>
+             <p className="card-text">managing and sending large quantities of mail at reduced rates, so it's an incredibly useful tool for any business looking to expand and reach new customers.</p>
+             <a href="#" className="btn btn-warning">Click here for more info</a>
               </div>
              </div>
-                </div>
+                </Tilt>
 
         </section>
 
