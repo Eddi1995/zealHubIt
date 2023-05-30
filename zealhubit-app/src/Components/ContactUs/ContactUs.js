6298d894943e00d5a14ac067e2f5615791b14639
Eddi1995 <edwinanthony513@gmail.com>
2023-05-31 02:27:35 +0530

    update9

--- zealhubit-app/src/Components/ContactUs/ContactUs.js
+++ zealhubit-app/src/Components/ContactUs/ContactUs.js
@@ -10,7 +10,38 @@ function ContactUs(){
                hellowone
             </div>
             <div>    {/*child2*/}
-              hellowtwo
+               <form >
+                <input  type="text" name="firstname" placeholder="Full Name*" required/>
+                <p></p>
+                <input type="email" name="compny Email" placeholder="Company Email"/>
+                <p></p>
+                <input type="number" name="mobile number" placeholder="Enter the placeholder"/>
+                <p></p>
+                <select>
+                  <option>Request for Services</option>
+                  <option>Dedicated Hiring</option>
+                  <option>Bussiness consulting</option>
+                  <option>Product Engineering</option>
+                  <option>Web/App Development</option>
+                  <option>Digital Marketing</option>
+                  <option>Cloud Services</option>
+                  <option>Data Analytics</option>
+                  <option>Maintenance and Support</option>
+                  <option>Media Query</option>
+                  <option>Job Enquiry</option>
+                  <option>Bulk mailing</option>
+                </select>
+                <p></p>
+                <textarea>
+
+                </textarea>
+                <h6>
+                zealHubIt needs the contact information you provide to us to contact you about our products and services.
+                 By submitting the form you agree to zeakHubIt Privacy Policy and Cookies & Internet Advertising.
+                  Don't worry, our privacy practices and commitment to protecting your privacy always.
+                </h6>
+                <button>Submit</button>
+               </form>
             </div>
          </section>
         </>
