8076a37b3b91c512d42e1c22c42d93c2ebfa244a
Eddi1995 <edwinanthony513@gmail.com>
2023-05-31 15:17:55 +0530

    updated9

--- zealhubit-app/src/Components/ContactUs/ContactUs.js
+++ zealhubit-app/src/Components/ContactUs/ContactUs.js
@@ -3,22 +3,33 @@ import Contactstyle from "./ContactUs.module.css"
 
 
 function ContactUs(){
+
     return(
         <>
          <section class={Contactstyle.ContactParent}>  {/*parent*/}
             <div>   {/*child1*/}
-               hellowone
+            <h2>We Love To Help You!</h2>
+               {/* <h2>We Love To Help You!</h2>
+               <h5>Send us a message and we will be in touch within on Business day</h5>
+               <div >
+                  <h6>We offer full-cycle technology solutions that streamline your digital transformation journey. 
+                     with cutting _edge technical expertise, zeakHubIt simplifies complex business challenges with 
+                     innovative solutions for 100+fastest-growing enterprises.</h6>
+               </div> */}
             </div>
+
             <div>    {/*child2*/}
+           
                <form >
-                <input  type="text" name="firstname" placeholder="Full Name*" required/>
+               <h3>Lets Get Started</h3>
+                <input  type="text" name="firstname" className="form-control" placeholder="Full Name*" required/>
                 <p></p>
-                <input type="email" name="compny Email" placeholder="Company Email"/>
+                <input type="email" name="compny Email" className="form-control" placeholder="Company Email"/>
                 <p></p>
-                <input type="number" name="mobile number" placeholder="Enter the placeholder"/>
+                <input type="number" name="mobile number" className="form-control" placeholder="Mobile Number*"/>
                 <p></p>
-                <select>
-                  <option>Request for Services</option>
+                <select className="form-control">
+                  <option>Request for Services?</option>
                   <option>Dedicated Hiring</option>
                   <option>Bussiness consulting</option>
                   <option>Product Engineering</option>
@@ -32,7 +43,7 @@ function ContactUs(){
                   <option>Bulk mailing</option>
                 </select>
                 <p></p>
-                <textarea>
+                <textarea className="form-control" placeholder="Comments">
 
                 </textarea>
                 <h6>
@@ -40,7 +51,7 @@ function ContactUs(){
                  By submitting the form you agree to zeakHubIt Privacy Policy and Cookies & Internet Advertising.
                   Don't worry, our privacy practices and commitment to protecting your privacy always.
                 </h6>
-                <button>Submit</button>
+                <button className="btn btn-warning ">Submit</button>
                </form>
             </div>
          </section>
