11344962fa5ab9b4729be11cae1fd25ec1fb5365
Eddi1995 <edwinanthony513@gmail.com>
2023-06-07 13:46:28 +0530

    updated 4

--- zealhubit-app/src/Components/Login/Login.js
+++ zealhubit-app/src/Components/Login/Login.js
@@ -10,21 +10,37 @@ class Login extends Component{
     passworderr:"",
   }
 
-  validatingform=()=>{
+  validatingform =()=>{
   let emailerr="";
   let passworderr="";
-  let pattern=new RegExp(/^([0-9][a-z][A-Z]).{4}$/)
+                         //Uncomment if you dont want required attribute
+                    
+//  let pattern1=new RegExp(/^[A-Z0-9+.]+@[A-Z0-9.-]+$/)  //    this is wrong check correct one
+
+  let pattern2=new RegExp(/^[a-zA-Z0-9]{6,}$/)
  
-   if(!pattern.test(this.state.email)){
-      emailerr="Minimum 4 char are required"
+  // if(!pattern1.test(this.state.email)){
+  //   emailerr="should contain @ and ."
+  // }
+  
+   if(!pattern2.test(this.state.password)){
+    passworderr="Password should be minimum 6 char";
    }
-    this.setState({emailerr:emailerr});
-    console.log(this.state.emailerr)
+   if(emailerr||passworderr){
+    this.setState({emailerr:emailerr,passworderr:passworderr});
+    return false;
+   }
+   else{
+    this.setState({emailerr:emailerr,passworderr:passworderr})
+    return false;
+   }
+    
   
   }
+  
 
   controllingLogin=(e)=>{
-      e.preventDefault();
+      // e.preventDefault();
       this.validatingform();
   }
 
@@ -39,10 +55,10 @@ class Login extends Component{
             <section class={Loginstyle.formControlling}>
               <form>
                 <span className="text-warning fs-3 p-1 bg-transparent">Login</span><span className="fs-3 p-3 bg-transparent">&#129488;</span>
-                <input type="email" name="email" placeholder="E-mail id*" className="form-control" onChange={this.inputValues} required/>
-                <p></p>
-                <input type="password" name="password" placeholder="Password*" className="form-control" onChange={this.inputValues} required/>
-                <p></p>
+                <input type="email" name="email" placeholder="E-mail id*" className="form-control" onChange={this.inputValues} required />
+                <p>{this.state.emailerr}</p>
+                <input type="password" name="password" placeholder="Password*" className="form-control" onChange={this.inputValues} />
+                <p>{this.state.passworderr}</p>
                 <h6>By continuing, I agree to the <a href="#">Terms of Use</a> & <a href="#">Privacy Policy</a></h6>
                 <button onClick={this.controllingLogin} className="btn btn-warning">Submit</button>
                 <h6 >Have trouble logging in? <a href="#">Get help</a></h6>
